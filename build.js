27949ebd1ae169b613581288452441f25ab53bdf
MUHAMAD CASDI <KNO2104136@smsfinance.co.id>
2024-09-25 13:45:30 +0700

    Feat: Initial package

--- build.js
+++ build.js
@@ -12,24 +12,6 @@ const buildOptions = {
     external: ['react', 'react-dom'],
 };
 
-const runBuild = async () => {
-    const ctx = await esbuild.build({
-        ...buildOptions,
-        watch: {
-            onRebuild(error, result) {
-                if (error) {
-                    console.error('Build failed:', error);
-                } else {
-                    console.log('Build succeeded:', result);
-                }
-            },
-        },
-    });
-    console.log('Watching for changes...');
-};
-
-if (process.argv.includes('--watch')) {
-    runBuild();
-} else {
-    esbuild.build(buildOptions).catch(() => process.exit(1));
-}
+esbuild.build(buildOptions).then(() => {
+    console.log('Build succeeded.');
+}).catch(() => process.exit(1));
